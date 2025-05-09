fb4f59a39dc6e72c796037258dacb501b38b2143
Nicolai Martini <152643549+NicolaiMartini@users.noreply.github.com>
2025-05-09 14:08:25 +0200

    Slow start working on the software part of the project.

---
+++ app.py
@@ -0,0 +1,53 @@
+"""
+This is a template for .py files,
+to ensure a similar and streamlined construction.
+"""
+
+##### IMPORTS
+import smbus3
+import gpiozero
+import threading
+import ultralytics
+
+from matplotlib.figure import Figure
+from picamera2 import Picamera2
+from sqlite3 import connection
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+from datetime import datetime
+
+##### PINS
+
+
+
+##### CONFIGURATIONS
+
+
+
+##### OBJECTS
+
+
+
+
+##### VARIABLES
+
+
+
+
+##### FUNCTIONS
+
+
+
+##### PROGRAM
+while True:
+	try:
+		app=Flask(__name__)
+		socketio=SocketIO(app)
+		
+        @app.route("/home")
+        def home():
+
+		if __name__==('__main__'):
+			socketio.run(app,host="0.0.0.0",debug=True)
+	except Exception as e:
+		print(e)
