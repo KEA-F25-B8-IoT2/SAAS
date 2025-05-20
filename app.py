6ea61e895494533e56b47d4ff31376a58560f8f2
Nicolai Martini <152643549+NicolaiMartini@users.noreply.github.com>
2025-05-20 17:14:29 +0200

    Major progress.
    Dynamic updates to website graph using socketio.
    Autostart of tmux with private wlan in one pane, and flask/socketio website in another pane.

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ The main python script for waste detection, analyzation and sorting.
 
 ##### IMPORTS
 import threading
+import base64
+import traceback
 
 from time import sleep
 from datetime import datetime
 from ultralytics import YOLO
+from sqlite3 import Connection
+from io import BytesIO
+from matplotlib.figure import Figure
 
 from picamera2 import Picamera2, Preview
 from flask import Flask, render_template
@@ -93,95 +98,169 @@ class step_motor(object):
 			control_pin.off()
 
 	def arm_degrees(self,desired_degrees):
+		""" How many degrees to rotate the arm. """
 		output_degrees=int(desired_degrees*(512/360))
 		return output_degrees
 
-	def arm_forward(self,deg):
-		self.arm_move(self.arm_degrees(deg), direction=CW)
+	def forward(self):
+		self.arm_move(self.arm_degrees(45), direction=CW)
 
-	def arm_backward(self,deg):
-		self.arm_move(self.arm_degrees(deg), direction=CCW)
+	def backward(self):
+		self.arm_move(self.arm_degrees(45), direction=CCW)
 
-class Sonar(object):
+class Sonar(DistanceSensor):
 	def __init__(self,pin_echo,pin_trigger):
 		""" Initialize the sonar. Input echo and trigger pins."""
-		self.echo=pin_echo
-		self.trigger=pin_trigger
-		
+		super().__init__(echo=pin_echo,trigger=pin_trigger)
 
-	def distance_cm=self.distance()
+	@property # @Property decorator to access the attribute instead of calling the function, like in the original distancesensor
+	def distance_cm(self):
+		""" Sonar defaults to return distance in m. Convert this to cm."""
+		return self.distance*100 # .distance is a constantly updating attribute, and not a function that should be called.
 
 
 ##### OBJECTS
-MOTOR_BELT=PWMLED(PIN_MOTOR_BELT)
-MOTOR_BELT.off()
-arm_plastic=step_motor(PIN_STEP1_IN1,PIN_STEP1_IN2,PIN_STEP1_IN3,PIN_STEP1_IN4)
-arm_paper=step_motor(PIN_STEP2_IN1,PIN_STEP2_IN2,PIN_STEP2_IN3,PIN_STEP2_IN4)
-arm_cardboard=step_motor(PIN_STEP3_IN1,PIN_STEP3_IN2,PIN_STEP3_IN3,PIN_STEP3_IN4)
+MOTOR_BELT=PWMLED(PIN_MOTOR_BELT) # Create instance, and make sure it's off.
+ARM_PLASTIC=step_motor(PIN_STEP1_IN1,PIN_STEP1_IN2,PIN_STEP1_IN3,PIN_STEP1_IN4)
+ARM_PAPER=step_motor(PIN_STEP2_IN1,PIN_STEP2_IN2,PIN_STEP2_IN3,PIN_STEP2_IN4)
+ARM_CARDBOARD=step_motor(PIN_STEP3_IN1,PIN_STEP3_IN2,PIN_STEP3_IN3,PIN_STEP3_IN4)
+SONAR_LEFTOVER=Sonar(PIN_SON1_ECHO,PIN_SON1_TRIG)
+SONAR_PLASTIC=Sonar(PIN_SON2_ECHO,PIN_SON2_TRIG)
+SONAR_PAPER=Sonar(PIN_SON3_ECHO,PIN_SON3_TRIG)
+SONAR_CARDBOARD=Sonar(PIN_SON4_ECHO,PIN_SON4_TRIG)
 
 
 # https://docs.ultralytics.com/guides/raspberry-pi/#test-the-camera
 ## Inference with Camera
-### Method 1
-####Initialize the Picamera2
 picam2 = Picamera2() # Instantiate
 picam2.preview_configuration.main.format = "RGB888" # Set format
 picam2.start() # Start camera
-model= YOLO("yolo11n.pt")
+model= YOLO("waste.pt")
 app=Flask(__name__)
 socketio=SocketIO(app)
 
 ##### FUNCTIONS
+def conv_start():
+	MOTOR_BELT.value=0.6
+
+def conv_stop():
+	MOTOR_BELT.off()
+
 def threaded_webserver():
 	socketio.run(app,host='0.0.0.0',port=44380)
 
-@app.route('/')
-def index():
-    return # Opdater til at bruge index.html
+def chart_bar():
+    amounts=[
+        db_select_last_rowid('leftover'),db_select_last_rowid('cardboard'),
+        db_select_last_rowid('paper'),db_select_last_rowid('plastic')
+        ] # Get last rowid from each table.
+    fig=Figure()
+    ax=fig.subplots()
+    x=['leftover','cardboard','paper','plastic'] # Create x-axis for graph
+    y=[amounts[0],amounts[1],amounts[2],amounts[3]] # Create y-axis for graph
+    bars=ax.bar(x,y,color=['#2ecc71', '#3498db', '#9b59b6', '#e74c3c']) # Create and colorise bars in graph
+    ax.tick_params(axis='x',which='both',rotation=30) # Is this necessary?
+    ax.set_xlabel("Waste type")
+    ax.set_ylabel("Pieces of waste")
+    ax.set_title("Waste sorting")
+    for bar in bars: # Insert number of waste items per bar
+        height=bar.get_height()
+        ax.text(bar.get_x() + bar.get_width()/2., height, f'{int(height)}', ha='center',va='bottom')
+    ax.plot(x,y)
+    for line in ax.get_lines():
+        line.remove() # Remove ugly blue line from the "usual" graph.
+    buf=BytesIO()
+    fig.savefig(buf,format="png")
+    chart_bar=base64.b64encode(buf.getbuffer()).decode("ascii")
+    return chart_bar # Return chart for later use
 
+def db_updated_socketio():
+    render_chart_bar=chart_bar()
+    socketio.emit('database_updated', render_chart_bar) # Emit socketio/flask to update chart bar
+
+def db_select_last_rowid(specified_table):
+    con=Connection('waste_sorting.db') # Connect to DB
+    cur=con.cursor()
+    sql=f"""SELECT rowid from {specified_table} ORDER BY rowid DESC LIMIT 1""" # Get "total amount of trash for this category" by fetching last rowid from desired table
+    cur.execute(sql) # Run sql-command
+    list_id=cur.fetchall() # Fetch from SQL-query
+    tuple_id=list_id[0] # Convert from single-item list-nested tuple, to single-item tuple
+    total_id, *useless = tuple_id # Convert tuple to single item, and pack rest into useless-variable
+    con.close()
+    return int(total_id) # Make sure total_id is int for future use
+
+@app.route('/') # One page website.
+def index():
+    return render_template('index.html',render_chart_bar=chart_bar()) # Start website with bar chart.
+
+def db_insert(specified_table):
+    con=Connection('waste_sorting.db')
+    cur=con.cursor()
+    timestamp=f"{datetime.now().strftime('%d-%m-%Y %H:%M:%S')}" # Get formatted timestamp
+    sql=f"""INSERT INTO {specified_table}(timestamp)VALUES(?)"""
+    cur.execute(sql,(timestamp,)) # Single item tuple needed or execute() will throw an error
+    con.commit()
+    con.close()
+    db_updated_socketio() # Emit to socketio that the db has been updated
+
+def total_amount_socketio():
+    total=db_select_last_rowid('leftover')+db_select_last_rowid('cardboard')+db_select_last_rowid('paper')+db_select_last_rowid('plastic')
+    socketio.emit('amount_updated',total)
 
 ##### PROGRAM
-def main():
-	global arm_activated, arm_default_position
-	web_server = threading.Thread(target=threaded_webserver, daemon=True)
-	web_server.start()
+def main(): # Main program
+	# global arm_activated, arm_default_position # Make sure variables are usable ## Is this useful or just clutter?
+	web_server = threading.Thread(target=threaded_webserver, daemon=True).start() # Start webserver as a background thread. 'daemon' makes sure the thread shuts down when main script shuts down.
 	while True:
-		try:
-			frame=picam2.capture_array() # Capture pic
-			results=model.predict(frame, conf=0.2, iou=0.3) # analyze
-			r=results[0] # save results
-			s=r.summary() # Summarise results
-			for res in s:
+		frame=picam2.capture_array() # Capture pic
+		results=model.predict(frame, conf=0.2, iou=0.3) # analyze
+		r=results[0] # save results
+		s=r.summary() # Summarise results
+		if s: # Only engage if something is detected. Avoids list index error.
+			for res in s: # Engage for every detected object
 				print(res['name'])
 				socketio.emit('detected_object', res['name'])
-				if arm_activated==True and arm_default_position==False:
-					sleep(5)
-					arm_paper.arm_backward(45)
-					arm_plastic.arm_backward(45)
-					arm_cardboard.arm_backward(45)
-					MOTOR_BELT.off()
-					arm_activated=False
-					arm_default_position=True
-				if res['name']=='person' and res['confidence'] >= 0.3 and arm_default_position==True:
-					MOTOR_BELT.value = 0.6
-					print(res['name'].upper())
-				if res['name']=='laptop' and res['confidence'] >= 0.3:
-					arm_activated=True
-					arm_default_position=False
-					print(res['name'].upper())
-					arm_paper.arm_forward(45) # 45 Can be changed to desired degree reotation
-					arm_plastic.arm_forward(45)
-					arm_cardboard.arm_forward()
-			sleep(3)
-		except Exception as e:
-			print(e)
-			break
-		except KeyboardInterrupt:
-			MOTOR_BELT.off()
-			break
+				total_amount_socketio()
+				if res['name'] not in ['cardboard', 'paper', 'plastic']:
+					print(res['name']," inserted into db")
+					# SONAR_LEFTOVER_START=SONAR_LEFTOVER.distance_cm() # Check current waste height in bin
+					db_insert('leftover') # Send timestamp to database for new deposit
+					# conv_start() # Enable conveyer belt
+					# if SONAR_LEFTOVER_START-SONAR_LEFTOVER.distance_cm()>=5: # If height of waste is increased by 5cm or more
+					# 	conv_stop() # Disable conveyer belt
+				else:
+					db_insert(res['name'])
+					# print(f"{res['name']} amount: {db_select_last_rowid(res['name'])}")
+					if res['name']=='cardboard':
+						print(res['name']," inserted into db")
+						# SONAR_CARDBOARD_START=SONAR_CARDBOARD.distance_cm()
+						# ARM_CARDBOARD.forward()
+						# conv_start()
+						# if SONAR_CARDBOARD_START-SONAR_CARDBOARD.distance_cm()>=5:
+						# 	conv_stop()
+						# 	ARM_CARDBOARD.backward()
+					if res['name']=='paper':
+						print(res['name']," inserted into db")
+						# SONAR_PAPER_START=SONAR_PAPER.distance_cm()
+						# ARM_PAPER.forward()
+						# conv_start()
+						# if SONAR_PAPER_START-SONAR_PAPER.distance_cm()>=5:
+						# 	conv_stop
+						# 	ARM_PAPER.backward()
+					if res['name']=='plastic':
+						print(res['name']," inserted into db")
+						# SONAR_PLASTIC_START=SONAR_PLASTIC.distance_cm()
+						# ARM_PLASTIC.forward()
+						# conv_start()
+						# if SONAR_PLASTIC_START-SONAR_PLASTIC.distance_cm()>=5:
+						# 	conv_stop()
+						# 	ARM_PLASTIC.backward()
+		sleep(3) # Sleep 3 seconds before analysing live-view again
+
 
 if __name__=='__main__':
-	try:
+	try: # For error handling
 		main()
-	except Exception as e:
-		print(e)
+	except Exception:
+		print(f"Traceback: \n{traceback.format_exc()}") # Make sure breaking error is printed for debugging.
+		conv_stop() # Disable transport belt in case it's running.
